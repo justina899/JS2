4fb0340bb906119414ee914bf1c426ded6e71cb0
Justina <justina.babrauskiene@gmail.com>
2022-10-26 10:00:10 +0300

    1 done

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("1 uzd");
+let vardas = "Jonas";
+let pavarde = "Jonaitis";
+if(vardas > pavarde){
+console.log(vardas);
+}else {
+    console.log(pavarde);
+}
+//--------------------------------
